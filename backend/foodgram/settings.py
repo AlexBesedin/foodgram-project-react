2ff0cdf04c583dd784248d5368f37cfe34a4fd69
AlexBeszedin <alebesedin@yandex.ru>
2023-03-22 15:14:28 +0100

    В settings добавил os.getenv

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE'),
